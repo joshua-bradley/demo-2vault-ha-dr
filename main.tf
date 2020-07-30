1e5e6f4f1df6ecd89e200a07962dd0cec467d4ba
joshua-bradley <jb@joshuabradley.io>
2020-07-30 16:57:30 -0700

    root commit

---
+++ main.tf
@@ -0,0 +1,196 @@
+###
+# DEPLOY A VAULT SERVER CLUSTER, AN ELB, AND A CONSUL SERVER CLUSTER IN AWS
+# This example uses the hashicorp vault-cluster and vault-elb modules to
+# deploy a Vault cluster in AWS with an Elastic Load Balancer (ELB) in front
+# of it. This cluster uses a seperate Consul cluster as its storage backend.
+###
+
+###
+# Requirements for the terraform provisioner
+# This module has been updated with 0.12 syntax, which means it is no longer compatible with any versions below 0.12.
+###
+terraform {
+  required_version = ">= 0.12"
+}
+
+###
+# Use the latest publicly available pre-built vault-consul ami for the base of each asg
+# !! WARNING !! These amis are meant only as a convenience when initially testing this repo. Do NOT use them in production
+# as they contain TLS certificate files that are publicly available from the Module repo containing their source code.
+###
+data "aws_ami" "vault_consul" {
+  most_recent = true
+
+  # If we change the AWS Account in which test are run, update this value.
+  owners = ["562637147889"]
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "is-public"
+    values = ["true"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["vault-consul-ubuntu-*"]
+  }
+}
+
+###
+# Deploy Vault server cluster
+###
+module "vault_cluster" {
+  source = "github.com/hashicorp/terraform-aws-vault//modules/vault-cluster?ref=v0.13.8"
+
+  cluster_name  = var.vault_cluster_name
+  cluster_size  = var.vault_cluster_size
+  instance_type = var.vault_instance_type
+
+  ami_id    = var.ami_id == null ? data.aws_ami.vault_consul.image_id : var.ami_id
+  user_data = data.template_file.user_data_vault_cluster.rendered
+
+  vpc_id     = data.aws_vpc.default.id
+  subnet_ids = data.aws_subnet_ids.default.ids
+
+  health_check_type = "EC2"
+
+  # To make testing easier, we allow requests from any IP address here but in a production deployment, we *strongly*
+  # recommend you limit this to the IP address ranges of known, trusted servers inside your VPC.
+
+  allowed_ssh_cidr_blocks              = ["0.0.0.0/0"]
+  allowed_inbound_cidr_blocks          = ["0.0.0.0/0"]
+  allowed_inbound_security_group_ids   = []
+  allowed_inbound_security_group_count = 0
+  ssh_key_name                         = var.ssh_key_name
+}
+
+# Consul iam policies to allow vault cluster to discover consul backend
+module "consul_iam_policies_servers" {
+  source = "github.com/hashicorp/terraform-aws-consul.git//modules/consul-iam-policies?ref=v0.7.6"
+
+  iam_role_id = module.vault_cluster.iam_role_id
+}
+
+# This script will configure and start Vault
+data "template_file" "user_data_vault_cluster" {
+  template = file("${path.module}/templates/user-data-vault.sh")
+
+  vars = {
+    aws_region               = data.aws_region.current.name
+    consul_cluster_tag_key   = var.consul_cluster_tag_key
+    consul_cluster_tag_value = var.consul_cluster_name
+  }
+}
+
+# Consul sg for allowing internal communications between agents and servers
+module "security_group_rules" {
+  source = "github.com/hashicorp/terraform-aws-consul.git//modules/consul-client-security-group-rules?ref=v0.7.6"
+
+  security_group_id = module.vault_cluster.security_group_id
+
+  # To make testing easier, we allow requests from any IP address here but in a production deployment, we *strongly*
+  # recommend you limit this to the IP address ranges of known, trusted servers inside your VPC.
+
+  allowed_inbound_cidr_blocks = ["0.0.0.0/0"]
+}
+
+# Deploy an elb for public access to vault
+module "vault_elb" {
+  # When using these modules in your own templates, you will need to use a Git URL with a ref attribute that pins you
+  # to a specific version of the modules, such as the following example:
+  source = "github.com/hashicorp/terraform-aws-vault//modules/vault-elb?ref=v0.13.8"
+  #   source = "./modules/vault-elb"
+
+  name = var.vault_cluster_name
+
+  vpc_id     = data.aws_vpc.default.id
+  subnet_ids = data.aws_subnet_ids.default.ids
+
+  # Associate the ELB with the instances created by the Vault Autoscaling group
+  vault_asg_name = module.vault_cluster.asg_name
+
+  # To make testing easier, we allow requests from any IP address here but in a production deployment, we *strongly*
+  # recommend you limit this to the IP address ranges of known, trusted servers inside your VPC.
+  allowed_inbound_cidr_blocks = ["0.0.0.0/0"]
+
+  # In order to access Vault over HTTPS, we need a domain name that matches the TLS cert
+  create_dns_entry = var.create_dns_entry
+
+  # Terraform conditionals are not short-circuiting, so we use join as a workaround to avoid errors when the
+  # aws_route53_zone data source isn't actually set: https://github.com/hashicorp/hil/issues/50
+  hosted_zone_id = var.create_dns_entry ? join("", data.aws_route53_zone.selected.*.zone_id) : ""
+
+  domain_name = var.vault_domain_name
+}
+
+# Look up the Route 53 Hosted Zone by domain name
+data "aws_route53_zone" "selected" {
+  count = var.create_dns_entry ? 1 : 0
+  name  = "${var.hosted_zone_domain_name}."
+}
+
+###
+# Deploy consul server cluster
+###
+module "consul_cluster" {
+  source = "github.com/hashicorp/terraform-aws-consul.git//modules/consul-cluster?ref=v0.7.6"
+
+  cluster_name  = var.consul_cluster_name
+  cluster_size  = var.consul_cluster_size
+  instance_type = var.consul_instance_type
+
+  # The EC2 Instances will use these tags to automatically discover each other and form a cluster
+  cluster_tag_key   = var.consul_cluster_tag_key
+  cluster_tag_value = var.consul_cluster_name
+
+  ami_id    = var.ami_id == null ? data.aws_ami.vault_consul.image_id : var.ami_id
+  user_data = data.template_file.user_data_consul.rendered
+
+  vpc_id     = data.aws_vpc.default.id
+  subnet_ids = data.aws_subnet_ids.default.ids
+
+  # To make testing easier, we allow Consul and SSH requests from any IP address here but in a production
+  # deployment, we strongly recommend you limit this to the IP address ranges of known, trusted servers inside your VPC.
+
+  allowed_ssh_cidr_blocks     = ["0.0.0.0/0"]
+  allowed_inbound_cidr_blocks = ["0.0.0.0/0"]
+  ssh_key_name                = var.ssh_key_name
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# THE USER DATA SCRIPT THAT WILL RUN ON EACH CONSUL SERVER WHEN IT'S BOOTING
+# This script will configure and start Consul
+# ---------------------------------------------------------------------------------------------------------------------
+
+data "template_file" "user_data_consul" {
+  template = file("${path.module}/examples/root-example/user-data-consul.sh")
+
+  vars = {
+    consul_cluster_tag_key   = var.consul_cluster_tag_key
+    consul_cluster_tag_value = var.consul_cluster_name
+  }
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# DEPLOY THE CLUSTERS IN THE DEFAULT VPC AND AVAILABILITY ZONES
+# Using the default VPC and subnets makes this example easy to run and test, but it means Consul and Vault are
+# accessible from the public Internet. In a production deployment, we strongly recommend deploying into a custom VPC
+# and private subnets. Only the ELB should run in the public subnets.
+# ---------------------------------------------------------------------------------------------------------------------
+
+data "aws_vpc" "default" {
+  default = var.use_default_vpc
+  tags    = var.vpc_tags
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+  tags   = var.subnet_tags
+}
+
+data "aws_region" "current" {
+}
