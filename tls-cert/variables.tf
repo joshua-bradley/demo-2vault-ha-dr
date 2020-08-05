bcbf7526ce87b05876489b928883cd3da0558825
joshua-bradley <jb@joshuabradley.io>
2020-08-05 01:02:36 -0700

    update tls certs in ami(s)

--- tls-cert/variables.tf
+++ tls-cert/variables.tf
@@ -7,7 +7,7 @@ variable "owner" {
 variable "dns_names" {
   description = "List of DNS names for which the certificate will be valid (e.g. vault.service.consul, foo.example.com)."
   type        = list(string)
-  default     = ["*.*.elb.amazonaws.com", "vault.service.consul", "vault.example.com"]
+  default     = ["*.us-west-2.elb.amazonaws.com", "*.eu-west-2.elb.amazonaws.com", "vault.service.consul", "vault.example.com"]
 }
 
 variable "ip_addresses" {
